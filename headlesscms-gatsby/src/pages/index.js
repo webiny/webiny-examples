f8be3aa6b2637289a938b6a9401a0443060caf55
Fsalker <andreiii500@yahoo.com>
2020-05-21 23:59:44 +0300

    feat: improve UI

--- headlesscms-gatsby/src/pages/index.js
+++ headlesscms-gatsby/src/pages/index.js
@@ -1,21 +1,36 @@
 import React from "react"
-
 import Layout from "../components/layout"
 
 const IndexPage = ({data}) => {
   const blogPosts = data.webinyHeadlessCms.listBlogPosts.data
-  const BlogPosts = blogPosts.map(post => (
+  const mainBlogPosts = blogPosts.slice(0, 2)
+  const otherBlogPosts = blogPosts.slice(2)
+
+  const MainBlogPosts = mainBlogPosts.map(post => (
     <div>
       <h1>{post.title}</h1>
       <p>{post.body}</p>
     </div>
   ))
 
-  console.log(blogPosts)
+  const OtherBlogPosts = otherBlogPosts
+    ? (
+        <div style={{marginTop: "64px"}}>
+          <h4>You might also like...</h4>
+            {otherBlogPosts.map(post => (
+              <div style={{ width: "300px", display: "inline-block", verticalAlign: "top" }}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+              </div>
+            ))}
+        </div>
+    )
+    : ""
 
   return (
     <Layout>
-      {BlogPosts}
+      {MainBlogPosts}
+      {OtherBlogPosts}
     </Layout>
   )
 }
