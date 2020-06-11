7a9c3e8953c33cc3d5dfbc39fd53faa2d2f546dc
Fsalker <Andreiii500@yahoo.com>
2020-06-11 17:30:11 +0300

    feat(headlesscms-gatsby): add comments

--- headlesscms-gatsby/src/pages/index.js
+++ headlesscms-gatsby/src/pages/index.js
@@ -2,9 +2,12 @@ import React from "react"
 import { graphql } from 'gatsby'
 import Layout from "../components/layout"
 
+// The IndexPage component that renders our blog posts
 const IndexPage = ({data}) => {
+  // GraphQL queried data is automatically inserted into the `data` parameter used below
   const blogPosts = data.webinyHeadlessCms.listBlogPosts.data
 
+  // We render a nice list of blog posts
   const BlogPosts = blogPosts.map(post => (
     <div key={`post-${post.id}`}>
       <h1>{post.title}</h1>
@@ -13,6 +16,7 @@ const IndexPage = ({data}) => {
   ))
 
   return (
+    {/* We use Gatsby's Layout to make our Blog look nice */}
     <Layout>
       {BlogPosts}
     </Layout>
@@ -21,6 +25,7 @@ const IndexPage = ({data}) => {
 
 export default IndexPage
 
+// A GraphQL query that fetches our blogs' data
 export const query = graphql`{
   webinyHeadlessCms {
     listBlogPosts {
