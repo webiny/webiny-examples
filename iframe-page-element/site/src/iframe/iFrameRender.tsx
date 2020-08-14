17de144ea2c7da91886650d68f6e0dab8e1acac0
Ashutosh <bhardwajashu96@hotmail.com>
2020-08-14 20:13:31 +0530

    fix: add missing `ElementRoot` in `iFrameRender`
    site app

--- iframe-page-element/site/src/iframe/iFrameRender.tsx
+++ iframe-page-element/site/src/iframe/iFrameRender.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { css } from "emotion";
+import { ElementRoot } from "@webiny/app-page-builder/render/components/ElementRoot";
+
 const outerWrapper = css({
     boxSizing: "border-box"
 });
@@ -12,7 +14,8 @@ const innerWrapper = css({
     paddingBottom: 0
 });
 
-const IFrame = ({ data }) => {
+const IFrame = ({ element }) => {
+    const { data } = element;
     // If the user didn't enter a URL, let's show a simple message.
     if (!data.iframe.url) {
         return <div>IFrame URL is missing.</div>;
@@ -20,11 +23,12 @@ const IFrame = ({ data }) => {
 
     // Otherwise, let's render the iframe.
     return (
-        <div
+        <ElementRoot
             className={
                 "webiny-pb-base-page-element-style webiny-pb-page-element-embed-iframe " +
                 outerWrapper
             }
+            element={element}
         >
             <div className={innerWrapper}>
                 <div
@@ -32,7 +36,7 @@ const IFrame = ({ data }) => {
                 />
                 <iframe src={data.iframe.url} width="100%" height={data.iframe.height} />
             </div>
-        </div>
+        </ElementRoot>
     );
 };
 
