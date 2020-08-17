2aa068e24ea795bdb6340f4f2fcf0feb30490ff8
Ashutosh <bhardwajashu96@hotmail.com>
2020-08-17 10:24:04 +0530

    feat: register `CSS class` plugin

--- css-class-page-element-setting/admin/src/App.tsx
+++ css-class-page-element-setting/admin/src/App.tsx
@@ -1,10 +1,12 @@
 import adminTemplate from "@webiny/app-template-admin-full";
 import "./App.scss";
+import customPlugins from "./plugins";
 
 export default adminTemplate({
     cognito: {
         region: process.env.REACT_APP_USER_POOL_REGION,
         userPoolId: process.env.REACT_APP_USER_POOL_ID,
         userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID
-    }
+    },
+    plugins: customPlugins
 });
