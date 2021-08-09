b084c66bfd481eddc3f77e47ff77187d505770d6
Ewan-Roberts <jojothegreat694@hotmail.com>
2021-08-09 13:15:31 -0300

    chore: remove comments to clean up file for tutorial

--- extend-admin-area/apps/admin/code/src/index.ts
+++ extend-admin-area/apps/admin/code/src/index.ts
@@ -18,58 +18,22 @@ import scaffoldsPlugins from "./scaffolds";
 import mp4Plugin from "./plugins/mp4Plugin";
 
 plugins.register([
-    /**
-     * Base app plugins (files, images).
-     */
     basePlugins,
-    /**
-     * ApolloClient link plugins.
-     */
     apolloLinkPlugins,
-    /**
-     * Complete admin app UI.
-     */
     adminPlugins,
-    /**
-     * Renders a welcome screen with useful links at "/".
-     */
     welcomeScreenPlugins(),
-    /**
-     * Handles location paths that don't have a corresponding route.
-     */
     routeNotFound,
-    /**
-     * Internationalization app.
-     */
     i18nPlugins,
-    /**
-     * Enables storing content (pages, forms, content, ...) in multiple locales.
-     */
     i18nContentPlugins,
-    /**
-     * Security app and authentication plugins.
-     */
     securityPlugins,
-    /**
-     * Page Builder app.
-     */
     pageBuilderPlugins,
-    /**
-     * Form Builder app.
-     */
     formBuilderPlugins,
-    /**
-     * Headless CMS app.
-     */
     headlessCmsPlugins,
-    /**
-     * App theme controls page builder and form builder layouts, styles, etc.
-     */
     theme(),
-    /**
-     * Plugins created via scaffolding utilities.
-     */
     scaffoldsPlugins(),
+    /**
+     * Add mp4 plugin here
+     */
     mp4Plugin
 ]);
 
