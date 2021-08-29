d10376f1b84096527a70b3d709fc44b1b05be43f
Adrian Smijulj <adrian1358@gmail.com>
2021-08-29 19:44:45 +0200

    fix: correct config

--- create-custom-application/pinterest-clone/app/code/webiny.config.ts
+++ create-custom-application/pinterest-clone/app/code/webiny.config.ts
@@ -1,7 +1,6 @@
 import invariant from "invariant";
 import { startApp, buildApp } from "@webiny/project-utils";
 import { getStackOutput } from "@webiny/cli-plugin-deploy-pulumi/utils";
-import AntdScssThemePlugin from "antd-scss-theme-plugin";
 
 // Exports fundamental start (watch) and build commands.
 // Within both commands, we rely on the deployed GraphQL API (project-applications-path/api) and its stack
@@ -23,59 +22,17 @@ export default {
             invariant(options.env, NO_ENV_MESSAGE);
             Object.assign(
                 process.env,
-                getStackOutput("project-applications-path/api", options.env, MAP)
+                getStackOutput("pinterest-clone/api", options.env, MAP)
             );
 
             // Starts local application development.
-            await startApp(
-                {
-                    ...options,
-                    webpack(config) {
-                        config.plugins.push(
-                            new AntdScssThemePlugin(__dirname + "/src/styles/theme.scss")
-                        );
-
-                        config.module.rules.push({
-                            test: /\.less$/,
-                            use: [
-                                {
-                                    loader: "style-loader",
-                                    options: {
-                                        sourceMap: process.env.NODE_ENV !== "production"
-                                    }
-                                },
-                                {
-                                    loader: "css-loader",
-                                    options: {
-                                        importLoaders: 1,
-                                        sourceMap: process.env.NODE_ENV !== "production"
-                                    }
-                                },
-                                "less-loader"
-                            ]
-                        });
-
-                        return config;
-                    },
-                    babel(config) {
-                        config.plugins.push([
-                            require.resolve("babel-plugin-import"),
-                            {
-                                libraryName: "antd",
-                                style: true
-                            }
-                        ]);
-                        return config;
-                    }
-                },
-                context
-            );
+            await startApp(options, context);
         },
         async build(options, context) {
             invariant(options.env, NO_ENV_MESSAGE);
             Object.assign(
                 process.env,
-                getStackOutput("project-applications-path/api", options.env, MAP)
+                getStackOutput("pinterest-clone/api", options.env, MAP)
             );
 
             // Creates a production build of your application, ready to be deployed to
