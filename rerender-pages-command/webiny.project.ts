91abc1c8a9fb9e1e964a12231af6835608ec66de
Adrian Smijulj <adrian1358@gmail.com>
2021-09-02 13:21:41 +0200

    wip: add rerender page via custom CLI command example

---
+++ rerender-pages-command/webiny.project.ts
@@ -0,0 +1,42 @@
+import cliWorkspaces from "@webiny/cli-plugin-workspaces";
+import cliPulumiDeploy from "@webiny/cli-plugin-deploy-pulumi";
+import cliPageBuilder from "@webiny/api-page-builder/cli";
+import cliAwsTemplate from "@webiny/cwp-template-aws/cli";
+
+// Scaffolds.
+import cliScaffold from "@webiny/cli-plugin-scaffold";
+import cliScaffoldFullStackApp from "@webiny/cli-plugin-scaffold-full-stack-app";
+import cliScaffoldGraphQlApi from "@webiny/cli-plugin-scaffold-graphql-api";
+import cliScaffoldExtendGraphQlApi from "@webiny/cli-plugin-scaffold-graphql-service";
+import cliScaffoldAdminModule from "@webiny/cli-plugin-scaffold-admin-app-module";
+import cliScaffoldReactApp from "@webiny/cli-plugin-scaffold-react-app";
+import cliScaffoldReactComponent from "@webiny/cli-plugin-scaffold-react-component";
+import cliScaffoldCiCd from "@webiny/cli-plugin-scaffold-ci";
+
+import rerenderPages from "./scripts/rerenderPages";
+
+export default {
+    template: "@webiny/cwp-template-aws@5.13.0",
+    name: "latest5130",
+    cli: {
+        plugins: [
+            cliWorkspaces(),
+            cliPulumiDeploy(),
+            cliPageBuilder(),
+            cliAwsTemplate(),
+
+            // Scaffolds.
+            cliScaffold(),
+            cliScaffoldFullStackApp(),
+            cliScaffoldGraphQlApi(),
+            cliScaffoldExtendGraphQlApi(),
+            cliScaffoldAdminModule(),
+            cliScaffoldReactApp(),
+            cliScaffoldReactComponent(),
+            cliScaffoldCiCd(),
+
+            // We added the new CLI command plugin here.
+            rerenderPages
+        ]
+    }
+};
