f5eb58cc97aa29e6e8f9df76f4f8fe08a3b88c19
Adrian Smijulj <adrian1358@gmail.com>
2021-09-15 15:24:08 +0200

    wip: security - gql api - user pool / domain

---
+++ create-custom-application/05-security/05.02-cloud-infrastructure/05.02.01-adding-user-pool-and-user-pool-domain/pinterest-clone/api/code/graphql/src/plugins/scaffolds/pins/types.ts
@@ -0,0 +1,11 @@
+export interface PinEntity {
+    PK: string;
+    SK: string;
+    id: string;
+    title: string;
+    description?: string;
+    coverImage?: string;
+    createdOn: string;
+    savedOn: string;
+    webinyVersion: string;
+}
