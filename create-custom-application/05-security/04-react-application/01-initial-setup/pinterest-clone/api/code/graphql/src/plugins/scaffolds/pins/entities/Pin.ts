2f3c6b7c6058192966628fb14bb2c02a9603f310
Adrian Smijulj <adrian1358@gmail.com>
2021-09-15 18:12:13 +0200

    wip: security - react-app - initial setup

---
+++ create-custom-application/05-security/04-react-application/01-initial-setup/pinterest-clone/api/code/graphql/src/plugins/scaffolds/pins/entities/Pin.ts
@@ -0,0 +1,28 @@
+// https://github.com/jeremydaly/dynamodb-toolbox
+import { Entity } from "dynamodb-toolbox";
+import table from "./table";
+import { PinEntity } from "../types";
+
+/**
+ * Once we have the table, we define the PinEntity entity.
+ * If needed, additional entities can be defined using the same approach.
+ */
+export default new Entity<PinEntity>({
+    table,
+    name: "Pin",
+    timestamps: false,
+    attributes: {
+        PK: { partitionKey: true },
+        SK: { sortKey: true },
+        id: { type: "string" },
+        title: { type: "string" },
+        description: { type: "string" },
+        coverImage: { type: "string" },
+        createdOn: { type: "string" },
+        savedOn: { type: "string" },
+
+        // Will store current version of Webiny, for example "5.9.1".
+        // Might be useful in the future or while performing upgrades.
+        webinyVersion: { type: "string" }
+    }
+});
