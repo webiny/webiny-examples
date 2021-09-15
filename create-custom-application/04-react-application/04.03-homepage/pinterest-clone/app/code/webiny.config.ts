99d5e215c132a8f5914641c02f00c7b669d73194
Adrian Smijulj <adrian1358@gmail.com>
2021-09-15 11:35:57 +0200

    wip: react application / homepage

--- create-custom-application/04-react-application/04.03-homepage/pinterest-clone/app/code/webiny.config.ts
+++ create-custom-application/04-react-application/04.03-homepage/pinterest-clone/app/code/webiny.config.ts
@@ -3,15 +3,16 @@ import { startApp, buildApp } from "@webiny/project-utils";
 import { getStackOutput } from "@webiny/cli-plugin-deploy-pulumi/utils";
 
 // Exports fundamental start (watch) and build commands.
-// Within both commands, we rely on the deployed GraphQL API (project-applications-path/api) and its stack
+// Within both commands, we rely on the deployed GraphQL API (pinterest-clone/api) and its stack
 // output to retrieve the URL over which the GraphQL API is accessible. If needed, additional
 // information can be retrieved too, but remember to export it in the cloud infrastructure
 // code, in the following files:
-// - `project-applications-path/api/pulumi/dev/index.ts`
-// - `project-applications-path/api/pulumi/prod/index.ts`
+// - `pinterest-clone/api/pulumi/dev/index.ts`
+// - `pinterest-clone/api/pulumi/prod/index.ts`
 
-const MAP = {
-    REACT_APP_GRAPHQL_API_URL: "${graphqlApiUrl}"
+const API_MAP = {
+    REACT_APP_API_URL: "${apiUrl}",
+    REACT_APP_GRAPHQL_API_URL: "${apiUrl}/graphql"
 };
 
 const NO_ENV_MESSAGE = `Please specify the environment via the "--env" argument, for example: "--env dev".`;
@@ -20,14 +21,28 @@ export default {
     commands: {
         async watch(options, context) {
             invariant(options.env, NO_ENV_MESSAGE);
-            Object.assign(process.env, getStackOutput("pinterest-clone/api", options.env, MAP));
+            Object.assign(
+                process.env,
+                getStackOutput({
+                    folder: "pinterest-clone/api",
+                    env: options.env,
+                    map: API_MAP
+                })
+            );
 
             // Starts local application development.
             await startApp(options, context);
         },
         async build(options, context) {
             invariant(options.env, NO_ENV_MESSAGE);
-            Object.assign(process.env, getStackOutput("pinterest-clone/api", options.env, MAP));
+            Object.assign(
+                process.env,
+                getStackOutput({
+                    folder: "pinterest-clone/api",
+                    env: options.env,
+                    map: API_MAP
+                })
+            );
 
             // Creates a production build of your application, ready to be deployed to
             // a hosting provider of your choice, for example Amazon S3.
