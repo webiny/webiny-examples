f9e6d2dc44413150ba28f25b5e1737c8fafb344e
Swapnil M Mane <swapnilmmane@gmail.com>
2021-10-19 15:34:05 +0530

    fix: done the formatting changes and fix a typo Thanks Pavel

--- headless-cms/custom-field-plugin/api/secretTextFieldStoragePlugin.ts
+++ headless-cms/custom-field-plugin/api/secretTextFieldStoragePlugin.ts
@@ -1,17 +1,17 @@
 import { CmsModelFieldToStoragePlugin } from "@webiny/api-headless-cms/types";
-import cryptr from 'cryptr';
+import cryptr from "cryptr";
 
 export default (): CmsModelFieldToStoragePlugin<String> => ({
-    type: "cms-model-field-to-storage",
-    name: "cms-model-field-to-storage-address",
-    fieldType: "secret-text",
-    async toStorage({ value }) {
-        const encryptText = new cryptr('myTotalySecretKey').encrypt(value)
-        return {
-            value: encryptText
-        };
-    },
-    async fromStorage({ value }) {
-        return new cryptr('myTotalySecretKey').decrypt(value.value)
-    }
+  type: "cms-model-field-to-storage",
+  name: "cms-model-field-to-storage-address",
+  fieldType: "secret-text",
+  async toStorage({ value }) {
+    const encryptText = new cryptr("myTotallySecretKey").encrypt(value);
+    return {
+      value: encryptText
+    };
+  },
+  async fromStorage({ value }) {
+    return new cryptr('myTotallySecretKey').decrypt(value.value)
+  }
 });
--- headless-cms/custom-field-plugin/ui/secretTextFieldPlugin.tsx
+++ headless-cms/custom-field-plugin/ui/secretTextFieldPlugin.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { CmsEditorFieldTypePlugin } from "@webiny/app-headless-cms/types";
 
-const TextIcon: React.FunctionComponent = () => (<i>icon</i>);
+const TextIcon: React.FunctionComponent = () => <i>icon</i>;
 
-export default(): CmsEditorFieldTypePlugin => ({
-    type: "cms-editor-field-type",
-    name: "cms-editor-field-type-secret-text",
-    field: {
+export default (): CmsEditorFieldTypePlugin => ({
+  type: "cms-editor-field-type",
+  name: "cms-editor-field-type-secret-text",
+  field: {
+    type: "secret-text",
+    label: "Secret Text",
+    description: "Store encrypted text into the database.",
+    icon: <TextIcon />,
+    allowMultipleValues: false,
+    allowPredefinedValues: false,
+    multipleValuesLabel: "Use as a list of text values",
+    createField() {
+      return {
         type: "secret-text",
-        label: "Secret Text",
-        description: "The encrypted text will be stored in the database.",
-        icon: <TextIcon />,
-        allowMultipleValues: false,
-        allowPredefinedValues: false,
-        multipleValuesLabel: "Use as list of text",
-        createField() {
-            return {
-                type: "secret-text",
-                validation: [],
-                renderer: {
-                    name: ""
-                }
-            };
+        validation: [],
+        renderer: {
+          name: ""
         }
+      };
     }
+  }
 });
