7cc519ddfe52c868bdd04c9fdab55d8071781124
Pavel Denisjuk <pavel@webiny.com>
2022-02-05 13:06:22 +0100

    Update menus.tsx

--- extend-admin-area-security/apps/admin/code/src/plugins/scaffolds/carManufacturers/menus.tsx
+++ extend-admin-area-security/apps/admin/code/src/plugins/scaffolds/carManufacturers/menus.tsx
@@ -12,42 +12,47 @@ import { FullAccessPermission } from "@webiny/app-security/types";
 // Creating types for security permissions makes our code less error-prone and more readable.
 import { CarManufacturersPermission } from "./types";
 
+// We need a component which will perform security checks, and conditionally render menu items.
+const CarManufacturersMenu = ({ Menu, Item }) => {
+  const { get } = useSecurity();
+
+  // We get the "car-manufacturers" permission from current identity (logged-in user).
+  const permission = identity.getPermission<CarManufacturersPermission | FullAccessPermission>(
+    "car-manufacturers"
+  );
+
+  if (!permission) {
+    return null;
+  }
+
+  // Note that the received permission object can also be `{ name: "*" }`. If so, that
+  // means we are dealing with the super admin, who has unlimited access.
+  let hasAccess = permission.name === "*";
+  if (!hasAccess) {
+    // If not super admin, let's check if we have the "r" in the `rwd` property.
+    hasAccess =
+      permission.name === "car-manufacturers" && permission.rwd && permission.rwd.includes("r");
+  }
+
+  // Finally, if current identity doesn't have access, we immediately exit.
+  if (!hasAccess) {
+    return null;
+  }
+
+  return (
+    <Menu name="menu-car-manufacturers" label={"Car Manufacturers"} icon={<Icon />}>
+      <Item label={"Car Manufacturers"} path={"/car-manufacturers"} />
+    </Menu>
+  );
+};
+
 /**
  * Registers "Car Manufacturers" main menu item.
  */
 export default new MenuPlugin({
-    render({ Menu, Item }) {
-        const { identity } = useSecurity();
-
-        // We get the "car-manufacturers" permission from current identity (logged in user).
-        const permission = identity.getPermission<
-            CarManufacturersPermission | FullAccessPermission
-            >("car-manufacturers");
-
-        if (!permission) {
-            return null;
-        }
-
-        // Note that the received permission object can also be `{ name: "*" }`. If so, that
-        // means we are dealing with the super admin, who has unlimited access.
-        let hasAccess = permission.name === "*";
-        if (!hasAccess) {
-            // If not super admin, let's check if we have the "r" in the `rwd` property.
-            hasAccess =
-                permission.name === "car-manufacturers" &&
-                permission.rwd &&
-                permission.rwd.includes("r");
-        }
-
-        // Finally, if current identity doesn't have access, we immediately exit.
-        if (!hasAccess) {
-            return null;
-        }
-
-        return (
-            <Menu name="menu-car-manufacturers" label={"Car Manufacturers"} icon={<Icon />}>
-                <Item label={"Car Manufacturers"} path={"/car-manufacturers"} />
-            </Menu>
-        );
-    }
+  render(props) {
+    // We forward the menu plugin props to our custom component, which will be using hooks.
+    // This `render` function is _not_ a React component itself, so you can't use hooks directly in here.
+    return <CarManufacturersMenu {...props} />;
+  }
 });
