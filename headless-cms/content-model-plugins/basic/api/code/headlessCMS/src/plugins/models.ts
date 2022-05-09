6a68441d844759d8e7b9e7b9beb6efd1d018e02d
Swapnil M Mane <swapnilmmane@gmail.com>
2022-05-09 16:49:07 +0530

    docs: improved the examples as per the new docs on creating content model via code

--- headless-cms/content-model-plugins/basic/api/code/headlessCMS/src/plugins/models.ts
+++ headless-cms/content-model-plugins/basic/api/code/headlessCMS/src/plugins/models.ts
@@ -1,55 +1,100 @@
 import { CmsModelPlugin } from "@webiny/api-headless-cms/content/plugins/CmsModelPlugin";
-import { ContentGroupPlugin } from "@webiny/api-headless-cms/content/plugins/ContentGroupPlugin";
+import { CmsGroupPlugin } from "@webiny/api-headless-cms/content/plugins/CmsGroupPlugin";
 
 export default [
     // Defines a new "E-Commerce" content models group.
-    new ContentGroupPlugin({
-        id: "ecommerce",
-        name: "E-Commerce",
-        slug: "e-commerce",
-        icon: "fas/shopping-cart"
+    new CmsGroupPlugin({
+      id: "ecommerce",
+      name: "E-Commerce",
+      slug: "e-commerce",
+      icon: "fas/shopping-cart"
     }),
 
     // Defines a new "Product" content model.
     new CmsModelPlugin({
-        name: "Product",
-        modelId: "product",
-        group: {
-            id: "ecommerce",
-            name: "E-Commerce"
+      name: "Product",
+      modelId: "product",
+      group: {
+        id: "ecommerce",
+        name: "E-Commerce",
+      },
+      fields: [
+        {
+          id: "productName",
+          fieldId: "productName",
+          type: "text",
+          label: "Product Name",
+          helpText: "A short product name",
+          renderer: { name: "text-input" },
+          validation: [
+            {
+              name: "required",
+              message: "Value is required.",
+            },
+          ],
         },
-        fields: [
+        {
+          id: "productSKU",
+          fieldId: "productSKU",
+          type: "text",
+          label: "SKU",
+          placeholderText: "SKU = Stock Keeping Unit",
+          renderer: { name: "text-input" }
+        },
+        {
+          id: "productPrice",
+          fieldId: "productPrice",
+          type: "number",
+          label: "Price",
+          renderer: { name: "text-input" }
+        },
+        {
+          id: "productCategory",
+          fieldId: "productCategory",
+          type: "ref",
+          label: "Category",
+          renderer: { "name": "ref-input" },
+          "settings": {
+           "models": [
             {
-                id: "name",
-                fieldId: "name",
-                type: "text",
-                label: "Product Name",
-                helpText: "A short product name",
-                renderer: { name: "text-input" },
-                validation: [
-                    {
-                        name: "required",
-                        message: "Value is required."
-                    }
-                ]
-            },
-            {
-                id: "sku",
-                fieldId: "sku",
-                type: "text",
-                label: "SKU",
-                placeholderText: "SKU = Stock Keeping Unit",
-                renderer: { name: "text-input" }
-            },
-            {
-                id: "price",
-                fieldId: "price",
-                type: "number",
-                label: "Price",
-                renderer: { name: "text-input" }
+             "modelId": "category"
             }
-        ],
-        layout: [["name"], ["sku", "price"]],
-        titleFieldId: "name"
+           ]
+          }
+         }
+      ],
+      layout: [["productName"], ["productSKU", "productPrice"], ["productCategory"]],
+      titleFieldId: "productName"
+    }),
+
+    // Defines a new "Category" content model.
+    new CmsModelPlugin({
+      name: "Category",
+      modelId: "category",
+      group: {
+        id: "ecommerce",
+        name: "E-Commerce"
+      },
+      fields: [
+        {
+          id: "categoryName",
+          fieldId: "categoryName",
+          type: "text",
+          label: "Category Name",
+          renderer: { name: "text-input" },
+          validation: [
+            {
+              name: "required",
+              message: "Value is required.",
+            },
+            {
+              name: "unique",
+              message: "Field value must be unique."
+            }
+          ]
+        }
+      ],
+      layout: [["categoryName"]],
+      titleFieldId: "categoryName"
     })
-];
+  ]
