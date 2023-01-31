1256eba87a8c4dfe00f3e53005713289a9e6a24f
adrians5j <adrian@webiny.com>
2023-01-31 14:52:49 +0100

    fix: update code samples

--- page-builder/spacex-page-element/apps/theme/pageElements/spaceX/website.ts
+++ page-builder/spacex-page-element/apps/theme/pageElements/spaceX/website.ts
@@ -2,10 +2,10 @@ import { PbRenderElementPlugin } from "@webiny/app-page-builder/types";
 import { SpaceX } from "./SpaceX";
 
 const plugin = {
-    name: "pb-render-page-element-space-x",
-    type: "pb-render-page-element",
-    elementType: "spaceX",
-    render: SpaceX
+  name: "pb-render-page-element-space-x",
+  type: "pb-render-page-element",
+  elementType: "spaceX",
+  render: SpaceX
 } as PbRenderElementPlugin;
 
 export default plugin;
