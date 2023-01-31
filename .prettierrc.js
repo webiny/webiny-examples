07849294139e82e0014587e687b3e1064c82d589
adrians5j <adrian@webiny.com>
2023-01-31 14:51:29 +0100

    chore: add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    printWidth: 100,
+    trailingComma: "none",
+    arrowParens: "avoid",
+    tabWidth: 2,
+    overrides: [
+        {
+            files: "*.js",
+            options: {
+                tabWidth: 4
+            }
+        }
+    ]
+};
