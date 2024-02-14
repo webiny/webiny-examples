2d89344788ec42dc86d643f58d444ffa718049e6
Pavel Denisjuk <pavel@webiny.com>
2024-02-14 12:50:01 +0100

    feat: add Brandfolder integration examples

---
+++ file-manager/integrate-brandfolder-dam/5.37.x-5.39.0/apps/admin/src/Brandfolder.tsx
@@ -0,0 +1,85 @@
+import React, { useCallback, useEffect, useRef } from "react";
+import styled from "@emotion/styled";
+import { FileManagerFileItem, FileManagerRenderer, OverlayLayout } from "@webiny/app-admin";
+import { createComponentPlugin } from "@webiny/app-serverless-cms";
+import { PanelUISdk, AttachmentDto } from "@brandfolder-panel/sdk";
+
+const BrandfolderContainer = styled.div`
+    overflow-y: scroll;
+    height: inherit;
+`;
+
+const token = "{YOUR_API_KEY}";
+
+export const Brandfolder = createComponentPlugin(FileManagerRenderer, () => {
+    return function BrandfolderRenderer(props) {
+        const ref = useRef<HTMLDivElement | null>(null);
+        const sdk = useRef<PanelUISdk | undefined>(undefined);
+
+        /**
+         * Handle file selection. Once a file is selected in the Brandfolder UI, we need to map the data to the format required by Webiny File Manager.
+         */
+        const onSelect = useCallback((dto: AttachmentDto) => {
+            const keys = Object.keys(dto) as Array<keyof AttachmentDto>;
+            /**
+             * Map Brandfolder file DTO to Webiny File Manager file item.
+             * Only `id` and `src` are the required properties, `meta` is optional.
+             */
+            const fmDto: FileManagerFileItem = {
+                id: dto.id,
+                src: dto.url,
+                meta: keys.map(key => ({
+                    key,
+                    value: dto[key]
+                }))
+            };
+
+            if (props.onChange) {
+                /**
+                 * Webiny File Manager supports selection of single and multiple files.
+                 * If the File Manager is requesting multiple files, then we must return an array.
+                 */
+                if (props.multiple) {
+                    props.onChange([fmDto]);
+                } else {
+                    props.onChange(fmDto);
+                }
+            }
+
+            props.onClose && props.onClose();
+        }, []);
+
+        /**
+         * Set up the Brandfolder Panel UI SDK, and immediately open the UI (call `selectAttachments`).
+         */
+        useEffect(() => {
+            if (!ref.current) {
+                console.warn("Can't mount Brandfolder Panel! Missing target DOM element.");
+                return;
+            }
+
+            sdk.current = new PanelUISdk({
+                appName: "test",
+                anchorElement: ref.current,
+                authParameters: { token }
+            });
+
+            sdk.current.selectAttachments({
+                allowedMimeTypes: props.accept,
+                onSelect
+            });
+
+            return () => {
+                sdk.current && sdk.current.closePanel();
+            };
+        }, []);
+
+        return (
+            <OverlayLayout onExited={() => props.onClose && props.onClose()}>
+                <BrandfolderContainer>
+                    <div ref={ref} id="panel-root" />
+                </BrandfolderContainer>
+            </OverlayLayout>
+        );
+    };
+});
---
+++ file-manager/integrate-brandfolder-dam/README.md
@@ -0,0 +1,25 @@
+# Integrate Brandfolder DAM
+
+Brandfolder is a digital asset management (DAM) platform (https://brandfolder.com/). You can  integrate it with Webiny, by following the code examples found in the relevant folders.
+
+---
+
+Click the version of Webiny you're using, and it will take you to the corresponding code example folder:
+- [<= 5.39.0](./5.37.x-5.39.0)
+- [>= 5.39.1](./5.39.1)
+
+There's no difference in the integration itself between these 2 version ranges. The only difference is the utility that you import from Webiny, to create the component decorators. The Brandfolder integration code is identical.
+
+## How to use the example code?
+Copy the changes from the examples to your project, into the exact same files. Next time you run your Admin app, you'll see Brandfolder UI instead of our built-in File Manager. 
+
+> IMPORTANT: In all examples, you'll need to install the [Brandfolder Panel SDK](https://www.npmjs.com/package/@brandfolder-panel/sdk). 
+ 
+To add the dependency directly to the Admin app workspace, run the following command:
+
+```bash
+yarn workspace admin add @brandfolder-panel/sdk
+```
+
+## I need help!
+Get in touch on our [community Slack channel](https://www.webiny.com/slack/)! :)
