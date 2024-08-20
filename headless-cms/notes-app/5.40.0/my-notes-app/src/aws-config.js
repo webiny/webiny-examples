481f69549b15687b33cec72d2f31c031ded00d92
Swapnil M Mane <swapnilmmane@gmail.com>
2024-08-20 22:14:47 +0530

    Updated code with placehold details

--- headless-cms/notes-app/5.40.0/my-notes-app/src/aws-config.js
+++ headless-cms/notes-app/5.40.0/my-notes-app/src/aws-config.js
@@ -1,8 +1,8 @@
 const config = {
     "aws_project_region": "us-east-1",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_ZjhRbUv0x",
-    "aws_user_pools_web_client_id": "41b7ti9vfj8ea0suhad4uv018j",
+    "aws_user_pools_id": "us-east-1_XXXXXXXXX",
+    "aws_user_pools_web_client_id": "XXXXXXXXXXXXXXXXXXXXXXXXXX",
 };
 
 export default config;
