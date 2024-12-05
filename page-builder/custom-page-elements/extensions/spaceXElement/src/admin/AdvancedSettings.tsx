74175f20e82d65388548da6680eff546619bfd4d
adrians5j <adrian@webiny.com>
2024-12-05 09:05:04 +0100

    wip: custom page elements via extensions

--- page-builder/custom-page-elements/extensions/spaceXElement/src/admin/AdvancedSettings.tsx
+++ page-builder/custom-page-elements/extensions/spaceXElement/src/admin/AdvancedSettings.tsx
@@ -7,51 +7,32 @@ import { Cell, Grid } from "@webiny/ui/Grid";
 import { Select } from "@webiny/ui/Select";
 
 export const AdvancedSettings = () => {
-    // In order to construct the settings form, we're using the
-    // `@webiny/form`, `@webiny/ui`, and `@webiny/validation` packages.
-    const { submit } = useForm();
-    return (
-        <>
-            <Grid>
-                <Cell span={12}>
-                    <Bind name={"variables.type"}>
-                        <Select
-                            label={"Type"}
-                            description={"Chose the record type you want to query."}
-                        >
-                            <option value="rockets">Rockets</option>
-                            <option value="dragons">Dragons</option>
-                        </Select>
-                    </Bind>
-                </Cell>
-                <Cell span={12}>
-                    <Bind
-                        name={"variables.limit"}
-                        validators={validation.create("required,gte:0,lte:1000")}
-                    >
-                        <Input
-                            label={"Limit"}
-                            type="number"
-                            description={"Number of records to be returned."}
-                        />
-                    </Bind>
-                </Cell>
-                <Cell span={12}>
-                    <Bind
-                        name={"variables.offset"}
-                        validators={validation.create("required,gte:0,lte:1000")}
-                    >
-                        <Input
-                            label={"Offset"}
-                            type="number"
-                            description={"Amount of records to be skipped."}
-                        />
-                    </Bind>
-                </Cell>
-                <Cell span={12}>
-                    <ButtonPrimary onClick={submit}>Save</ButtonPrimary>
-                </Cell>
-            </Grid>
-        </>
-    );
+  // In order to construct the settings form, we're using the
+  // `@webiny/form`, `@webiny/ui`, and `@webiny/validation` packages.
+  const { submit } = useForm();
+  return (
+    <Grid>
+      <Cell span={12}>
+        <Bind name={"variables.type"}>
+          <Select label={"Type"} description={"Chose the record type you want to query."}>
+            <option value="rockets">Rockets</option>
+            <option value="dragons">Dragons</option>
+          </Select>
+        </Bind>
+      </Cell>
+      <Cell span={12}>
+        <Bind name={"variables.limit"} validators={validation.create("required,gte:0,lte:1000")}>
+          <Input label={"Limit"} type="number" description={"Number of records to be returned."} />
+        </Bind>
+      </Cell>
+      <Cell span={12}>
+        <Bind name={"variables.offset"} validators={validation.create("required,gte:0,lte:1000")}>
+          <Input label={"Offset"} type="number" description={"Amount of records to be skipped."} />
+        </Bind>
+      </Cell>
+      <Cell span={12}>
+        <ButtonPrimary onClick={submit}>Save</ButtonPrimary>
+      </Cell>
+    </Grid>
+  );
 };
