0636eecf3a0e4bf7c6a0c4d96a2f35a06647f88e
Swapnil M Mane <swapnilmmane@gmail.com>
2024-12-23 15:29:39 +0530

    feat: moved OpenAI API call from client to server side by extending the Headless CMS GraphQL API (#171)

--- headless-cms/smart-seo-open-ai/5.41.x/extensions/smartSeoOpenAi/api/src/index.ts
+++ headless-cms/smart-seo-open-ai/5.41.x/extensions/smartSeoOpenAi/api/src/index.ts
@@ -1,6 +1,9 @@
 import { Article } from "./Article"
+import { generateSeo } from "./generateSeo";
+
 export const createExtension = () => {
     return [
-        Article
+        Article,
+        generateSeo
     ];
 };
